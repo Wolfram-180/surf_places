299e818ff1d4e9bde501e9e34045dc9dedb7233f
Wolfram-180 <sergei.ulvis@gmail.com>
2022-07-11 15:30:06 +0300

    4-4-2 done

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -18,14 +18,29 @@ class _SightListScreenState extends State<SightListScreen> {
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         toolbarHeight: 150,
-        title: const Text(
-          maxLines: 2,
-          app_strings.AppStrings.appTitle,
-          textAlign: TextAlign.left,
-          style: TextStyle(
-            fontSize: 40,
-            color: app_colors.AppColors.appTextColor,
-            fontWeight: FontWeight.bold,
+        title: RichText(
+          text: TextSpan(
+            style: const TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              color: app_colors.AppColors.appTextColor,
+            ),
+            children: [
+              TextSpan(
+                text: app_strings.AppStrings.appTitle.substring(0, 1),
+                style: const TextStyle(color: Colors.green),
+              ),
+              TextSpan(
+                text: app_strings.AppStrings.appTitle.substring(1, 7),
+              ),
+              TextSpan(
+                text: app_strings.AppStrings.appTitle.substring(7, 8),
+                style: const TextStyle(color: Colors.yellow),
+              ),
+              TextSpan(
+                text: app_strings.AppStrings.appTitle.substring(8, 22),
+              ),
+            ],
           ),
         ),
         backgroundColor: app_colors.AppColors.appBckgrColor,
