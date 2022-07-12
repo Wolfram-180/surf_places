02e859f333e207f9f03bf704b59b7b333b7b21f8
Wolfram-180 <sergei.ulvis@gmail.com>
2022-07-12 12:54:17 +0300

    4-6

---
+++ lib/mocks.dart
@@ -0,0 +1,28 @@
+import 'package:surf_places/domain/sight.dart';
+
+final List<Sight> mocks = [
+  Sight(
+    name: 'Пляж в деревне Верхняя Кышка',
+    lat: 53.29,
+    lon: 44.58,
+    url: 'https://kyshka-plyazh.ru',
+    details: 'Центральный пляж областного значения в деревне Верхняя Кышка',
+    type: 'Пляж',
+  ),
+  Sight(
+    name: 'Грибная поляна в Видном',
+    lat: 63.79,
+    lon: 54.78,
+    url: 'https://grebi-v-vidnoye.ru',
+    details: 'Собирайте грибы сколько хотите',
+    type: 'Отдых в лесу',
+  ),
+  Sight(
+    name: 'Страх и ненависть в Расторгуево',
+    lat: 62.97,
+    lon: 53.79,
+    url: 'https://veseluha.ru',
+    details: 'Полоса препятствий для экстремалов',
+    type: 'Активный отдых',
+  ),
+];
